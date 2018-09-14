5ca299036eaab3adc0798578783f45dcac5de422
Brianne Duffy <duffymbrianne@gmail.com>
2018-09-14 16:22:03 -0600

    initial incomplete very poor start of es6 flashcards

---
+++ main.js
@@ -0,0 +1,11 @@
+const flashcards = {
+  data: [
+    { id: 1, question: 'Var is used as a way to define variables in ES6.', answer: false },
+    { id: 2, question: 'Javascript was written in 11 days.', answer: true },
+    { id: 3, question: 'Dave Jugnst hates Javascript.', answer: false },
+    { id: 4, question: 'Synchronous code will block execution of any other code until it has finished.', answer: true }, 
+    { id: 5, question: 'You can make promises in ECMAScript.', answer: true},
+    { id: 6, question: 'Nsync was introduced in ES2017.', answer: false}
+  ]
+}
+
